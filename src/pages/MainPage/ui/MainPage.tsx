d917bb786651738a9da6977bf4f9b2f8ba2a4759
Ilya Khorobrykh <horobryh17@gmail.com>
2022-10-19 13:29:00 +0300

    removed Counter from MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,17 +2,10 @@ import React, { ReactElement } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { Counter } from 'entities/Counter';
-
 const MainPage = (): ReactElement => {
     const { t } = useTranslation('main');
 
-    return (
-        <div>
-            {t('Main page', { ns: 'main' })}
-            <Counter />
-        </div>
-    );
+    return <div>{t('Main page', { ns: 'main' })}</div>;
 };
 
 export default MainPage;
