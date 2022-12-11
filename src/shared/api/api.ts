d1eb11e59d08a875a94b2518966105969c940a8a
Ilya Khorobrykh <horobryh17@gmail.com>
2022-12-11 12:54:41 +0300

    added vercel.json and added interceptor for axios requests

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,12 @@ import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localStorage';
 
 export const instance = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+});
+
+axios.interceptors.request.use(config => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+    }
+
+    return config;
 });
