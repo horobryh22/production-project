efd357e62434ab29cabe3255d33fd3402f8b797c
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-07-02 16:59:14 +0500

    fix script

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
-const cacheDir =  path.resolve(__dirname, '..', 'node_modules', '.cache');
+const cacheDir = path.resolve(__dirname, '..', 'node_modules', '.cache');
 
-fs.rmdirSync(cacheDir, {recursive: true});
+if (fs.existsSync(cacheDir)) {
+    fs.rmdirSync(cacheDir, { recursive: true });
+    console.log(`Cache directory ${cacheDir} has been removed.`);
+} else {
+    console.log(`Cache directory ${cacheDir} does not exist.`);
+}
 
 /* скрипт удаляет папку .cache после установки зависимостей с помощью npm i */
