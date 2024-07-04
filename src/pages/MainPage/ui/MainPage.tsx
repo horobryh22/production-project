fc6e447952863200c0729f85fb75c017ed198fb5
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-07-04 16:38:02 +0500

    123 lesson

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -7,7 +7,12 @@ import { Page } from '@/widgets/Page';
 const MainPage = (): ReactElement => {
     const { t } = useTranslation('main');
 
-    return <Page data-testid={'MainPage'}>{t('Main page', { ns: 'main' })}</Page>;
+    return (
+        <Page data-testid={'MainPage'}>
+            <div>1111111111111111</div>
+            {t('Main page', { ns: 'main' })}
+        </Page>
+    );
 };
 
 export default MainPage;
