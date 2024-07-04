c9dd1f5faded6070c5da0dafd42109819c06a30d
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-07-04 16:54:16 +0500

    123 lesson

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+# Переходим в директорию проекта
 cd ~/production-project
+
+# Обновляем код из репозитория
 git pull origin main
+
+# Собираем проект
 npm run build:prod
 
-rm -rf ~/../var/www/production-project/html
-mv ~/production-project/build ~/../var/www/production-project/html
+# Удаляем старую версию из директории назначения
+rm -rf /var/www/production-project/html
+
+# Перемещаем новую сборку в директорию назначения
+mv ~/production-project/build /var/www/production-project/html
+
+# Выводим сообщение об успешном завершении
+echo "Deployment completed successfully!"
