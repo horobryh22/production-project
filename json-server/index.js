6e8d554a33dfb5abcaa32779cacbbc50df34b962
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-07-04 11:15:50 +0500

    121 lesson

--- json-server/index.js
+++ json-server/index.js
@@ -59,8 +59,9 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
+const PORT = 8443;
 const httpServer = https.createServer(options, server);
 
-httpServer.listen(443, () => {
-    console.log('server is running on 443 port');
+httpServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
 });
